017d0aac12c14c99eee08f62e8a156bd54072c9b
RyanSonder <ryansonderpersonal@gmail.com>
2025-07-21 15:35:15 -0400

    fix conversion problem needing v3

--- scripts/rds_to_h5ad.R
+++ scripts/rds_to_h5ad.R
@@ -72,19 +72,36 @@ if (!file.exists(rds)) {
 
 # - Load the RDS file
 cat(sprintf("Loading RDS file: %s\n", rds))
-rds_data <- readRDS(rds)
+so <- readRDS(rds)
 
-# - Take the subset of the RDS data based on the identity
-cat(sprintf("Subsetting data for identity: %s\n", ident))
-subset_data <- rds_data[, rds_data@meta.data[[ meta_field ]] == ident ]
+# - Convert to V3 object (h5ad conversion does not work on V5)
+cat(sprintf("Converting Seurat object to V3\n"))
+so[["RNA3"]] <- as(so[["RNA"]], Class = "Assay")
+DefaultAssay(so) <- "RNA3"
+so[["RNA"]] <- NULL
+so <- RenameAssays(so, RNA3 = "RNA")
+
+# - Find list of barcodes
+cat(sprintf("Getting list of barcodes from Seurat object\n"))
+barcodes <- colnames(so)
+cat(sprintf("Discovered %d barcodes\n", length(barcodes)))
+
+# - Get the barcodes matching the sample ident
+cat(sprintf("Selecting barcodes matching %s\n", ident))
+bcs_subset <- grep(ident, barcodes, value = TRUE)
+cat(sprintf("Selected %d barcodes\n", length(bcs_subset)))
+
+# - Subset the Seurat object basted on the barcodes subset
+cat(sprintf("Subsetting Seurat object based on selected barcodes\n"))
+so_subset <- subset(so, cells = bcs_subset)
 
 # - Save the Seurat object to h5Seurat format
 cat(sprintf("Saving subset data to h5Seurat format: %s\n", out))
-SaveH5Seurat(subset_data, filename = out)
+SaveH5Seurat(so_subset, filename = out, overwrite = TRUE)
 
 # - Convert the h5Seurat file to h5ad format
 cat(sprintf("Converting h5Seurat to h5ad format: %s\n", glue("{out}.h5seurat")))
-Convert(glue("{out}.h5seurat"), dest = "h5ad")
+Convert(glue("{out}.h5seurat"), dest = "h5ad", overwrite = TRUE)
 
 cat(sprintf("Conversion complete. Output file: %s.h5ad\n", out))
 
